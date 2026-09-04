e0e3d73753824ef548c27ba61079b4e81ee7d78a
Cohen Herbert <66497306+CohenHerbert@users.noreply.github.com>
2026-09-04 16:34:13 -0700

    Update dependencies and install types

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -2,9 +2,8 @@ import express, { type Request, type Response } from "express";
 import jwt from "jsonwebtoken";
 import fs from "fs";
 import path from "path";
-import jwt from "jsonwebtoken";
 import multer from "multer";
-import { db } from "@/prisma/db";
+// import { db } from "@/prisma/db";
 
 const router = express.Router();
 
@@ -35,20 +34,34 @@ router.get("/", (req: Request, res: Response) => {
   });
 });
 
-router.post("/upload", upload.single("file"), (req: Request, res: Response) => {
-  const file = req.file;
+router.post(
+  "/upload",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    const file = req.file;
 
-  if (!file) {
-    return res.status(400).json({ error: "No file provided" });
-  }
+    if (!file) {
+      return res.status(400).json({ error: "No file provided" });
+    }
 
-  console.log(file.filename);
+    console.log(file.filename);
 
-  res.status(200).json({
-    filename: file.filename,
-    url: `http://localhost:3000/files/${file.filename}`,
-  });
-});
+    // const newFile = await db.orm.public.File.create({
+    //   originalName: file.filename,
+    //   storedName: "",
+    //   mimeType: "",
+    //   size: 1,
+    //   path: "",
+
+    //   ownerId: "",
+    // });
+
+    res.status(200).json({
+      filename: file.filename,
+      url: `http://localhost:3000/files/${file.filename}`,
+    });
+  },
+);
 
 router.get("/:filepath", (req: Request, res: Response) => {
   const token = req.headers["authorization"];
